9a64e1619d720f84ee42c05f56eedd5157ae63f0
GenKawamura <gen.kawamura@cern.ch>
2020-05-14 16:39:16 +0200

    Added restore script in tier2_ELK again

--- containers/tier2_ELK/kibana/restore_dashboard.sh
+++ containers/tier2_ELK/kibana/restore_dashboard.sh
@@ -7,15 +7,19 @@ cd $(dirname $0)
 kibana_port=$1
 [ -z "$kibana_port" ] && kibana_port=5601
 if [ ! -e /tmp/kibana_dashboard.$UID ]; then
-    curl -u elastic:changeme -k -XPOST "http://localhost:${kibana_port}/api/kibana/dashboards/import" -H 'Content-Type: application/json' -H "kbn-xsrf: true" -d @export.json
-    [ $? -ne 0 ] && exit -1
+    for f in $(echo -e "dcache\narcce\npbs")
+    do
+	curl -u elastic:changeme -k -XPOST "http://localhost:${kibana_port}/api/kibana/dashboards/import" -H 'Content-Type: application/json' -H "kbn-xsrf: true" -d @${f}_export.json
+	[ $? -ne 0 ] && exit -1
+	echo "Restored Dashboard [$f]"
+    done
     date > /tmp/kibana_dashboard.$UID
 fi
 
 ## For Elasticsearch
 elastic_url=$(grep elasticsearch.url /usr/share/kibana/config/kibana.yml | awk '{print $2}')
 [ -z "$elastic_url" ] && echo "elastic_url is null" && exit -1
-curl -XPUT "${elastic_url}/_template/dcache-billing" -H 'Content-Type: application/json' -d @mapping.json
+curl -XPUT "${elastic_url}/_template/dcache-billing" -H 'Content-Type: application/json' -d @dcache_mapping.json
 [ $? -ne 0 ] && "Failed: making template in [$elastic_url]" && exit -1
 
 echo "Success: making template in [$elastic_url]"
