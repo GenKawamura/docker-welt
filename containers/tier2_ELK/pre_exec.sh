d85fdeb4bcd310898c461207339bbe424d3cce43
GenKawamura <gen.kawamura@cern.ch>
2020-05-28 15:34:42 +0200

    Removed multiple Logstash instances in tier2_ELK

---
+++ containers/tier2_ELK/pre_exec.sh
@@ -0,0 +1,5 @@
+[ ! -e $workarea/grafana ] && mkdir -v $workarea/grafana
+chmod 1777 $workarea/grafana
+
+[ ! -e $workarea/elasticsearch_index_data ] && mkdir -v $workarea/elasticsearch_index_data
+chmod 1777 $workarea/elasticsearch_index_data
