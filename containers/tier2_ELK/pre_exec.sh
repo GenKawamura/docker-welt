58ca303b39ba68dab4309d0f271959077f5bc616
GenKawamura <gen.kawamura@cern.ch>
2020-06-01 23:14:18 +0200

    Testing Grafana in Tier2ELK

--- containers/tier2_ELK/pre_exec.sh
+++ containers/tier2_ELK/pre_exec.sh
@@ -1,5 +1,11 @@
-[ ! -e $workarea/grafana ] && mkdir -v $workarea/grafana
-chmod 1777 $workarea/grafana
+[ ! -e workarea ] && mkdir -v workarea
 
-[ ! -e $workarea/elasticsearch_index_data ] && mkdir -v $workarea/elasticsearch_index_data
-chmod 1777 $workarea/elasticsearch_index_data
+## Generating Elasticsearch workspace
+[ ! -e workarea/elasticsearch_index_data ] && mkdir -v workarea/elasticsearch_index_data
+chmod 1777 workarea/elasticsearch_index_data
+
+## Generating Grafana workapace
+[ ! -e workarea/grafana ] && mkdir -v workarea/grafana
+chmod 1777 workarea/grafana
+cp -v grafana/grafana.db workarea/grafana
+chmod 666 workarea/grafana/grafana.db
